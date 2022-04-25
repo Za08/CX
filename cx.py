aecf3d14c6cdb5d3c67696b3629b7658e9dc01d4
Za08 <95156875+Za08@users.noreply.github.com>
2022-04-25 17:08:03 +0800

    Create cx.py

---
+++ cx.py
@@ -0,0 +1,37 @@
+from sys import argv
+
+
+def arg_or_input(arg_name: str, input_tip: str) -> str:
+    for item in argv[1:]:
+        if item.startswith(arg_name + "="):
+            split = item.split("=")
+            return input(input_tip) if len(split) < 2 else split[1]
+    return input(input_tip)
+
+
+def cx(byte: bytes) -> bytes:
+    data = b''
+    for i, e in enumerate(byte):
+        if i % 2 != 0:
+            data += bytes([e ^ 0x64])
+        else:
+            data += bytes([e ^ 0x32])
+
+    return data[::-1]
+
+
+f_in = arg_or_input("file", "请输入要操作的文件(带后缀): ")
+f_out = arg_or_input("output", "请输入输出的文件名(带后缀): ")
+
+try:
+    with open(file=f_in, mode="rb") as f:
+        text = f.read()
+    print("已读取文件数据")
+    with open(file=f_out, mode="wb") as f:
+        f.write(cx(text))
+    print("已写入数据")
+    with open(file=f_in, mode="rb") as f:
+        text = f.read()
+
+except FileNotFoundError as e:
+    print("未找到文件: '" + f_in + "'!")
